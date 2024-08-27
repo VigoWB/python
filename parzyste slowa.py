8471300bcd332385274fd31137bfed11504385f5
Vigo <vigoar@tlen.pl>
2024-08-27 21:32:39 +0200

    27-08-2024

---
+++ parzyste slowa.py
@@ -0,0 +1,10 @@
+'''def ktoreSAparzyste():
+    for x in slowa_parzyste:
+        if len(slowa_parzyste) % 2 == 0:
+            print(len(slowa_parzyste))
+
+ktoreSAparzyste()'''
+
+if __name__ == '__main__':
+    slowa_parzyste = ["mama", "tata", "kot", "sosna", "buka"]
+    print(slowa_parzyste)
