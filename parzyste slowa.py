fbca2d402399cfa2fa79d2529acb6476514e1cc9
Vigo <vigoar@tlen.pl>
2024-08-28 19:00:41 +0200

    28

--- parzyste slowa.py
+++ parzyste slowa.py
@@ -6,5 +6,12 @@
 ktoreSAparzyste()'''
 
 if __name__ == '__main__':
-    slowa_parzyste = ["mama", "tata", "kot", "sosna", "buka"]
+    slowa_parzyste = ("mama", "tata", "kot", "sosenkana", "buka")
+    lista = len(slowa_parzyste)
     print(slowa_parzyste)
+    print(slowa_parzyste[3])
+    odpowiedz = []
+    for x in range(0, lista):
+        if x % 2 == 0:
+            odpowiedz.append(x)
+            print(odpowiedz)
