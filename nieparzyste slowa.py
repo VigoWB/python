f2ebad30cc14b2cc9a47d735143319bccbb87661
Vigo <vigoar@tlen.pl>
2024-08-30 07:19:30 +0200

    rano 30 poprawki

--- nieparzyste slowa.py
+++ nieparzyste slowa.py
@@ -1,14 +1,14 @@
 def wybieranko(listacala):
     odpowiedzi = []
     for slowo in listacala:
-        if len(slowo) % 2 == 1:
-            odpowiedzi.append(slowo)
         if type(slowo) != str:
+            odpowiedzi.append(slowo)
+            continue
+        if len(slowo) % 2 == 1:
             odpowiedzi.append(slowo)
     return odpowiedzi
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     listacala = ["kot", 2, "buka", 3.0, "goblin", 7, "jemioła"]
     listanowa = wybieranko(listacala)
     print(listanowa)
-print("towidac")
