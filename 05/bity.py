544b7b9266e5e58b7256ec7cb66f87d4253ef8f7
Vigo <vigoar@tlen.pl>
2024-10-23 07:43:22 +0200

    rano 23-paz

--- 05/bity.py
+++ 05/bity.py
@@ -1,14 +1,20 @@
 def na_bajty(liczba):
-    wynik =""
+    wynik = ""
     while liczba > 0:
         wynik = str(liczba % 2) + wynik
         liczba = liczba // 2
     return wynik
-def na_dziesiatki(wejscie):
-    wejscie = ""
-        for wynik in -len(wejscie):
-            2**wejscie + wynik
+
+def na_dziesiatki(bity: str):
+    dlugosc = len(bity)
+    wynik = 0
+    for index in range(0, dlugosc):
+        cos = dlugosc - index - 1
+        wynik += (2**cos) * int(bity[index])
     return wynik
-if __name__ == '__main__':
+
+
+
+if __name__ == "__main__":
     print(na_bajty(213))
-    print(na_dziesiatki(11010101))
+    print(na_dziesiatki("11010101"))
---
+++ Pętle.py
@@ -0,0 +1,24 @@
+def suma_listy(lista):
+    ile_pozycji = len(lista)
+    suma = 0
+    #for asd in lista:
+     #   suma = asd + suma
+    for asd in range(0, ile_pozycji):
+        suma = suma + lista[asd]
+    return suma
+
+def ile_wystapien(szukane, lista):
+    suma = 0
+    for asd in lista:
+        if asd == szukane:
+            suma += 1
+    return suma
+
+if __name__ == "__main__":
+    print(suma_listy([1, 2, 3, 4]))  # 10
+    print(suma_listy([999, 2135, 7845, 11565]))  # 22544
+    print(suma_listy([-7, 11, -4, 20]))  # 20
+    print(ile_wystapien(3, [1, 2, 3, 3, 3, 4, 3]))  # 4
+    print(ile_wystapien(1, [1, 0, 1, 0, 1, 0, 1]))  # 5
+    print(ile_wystapien(798, [0, 7, 9, 1, 11, 23, 77]))  # 0
+    print(ile_wystapien(666, []))  # 0
