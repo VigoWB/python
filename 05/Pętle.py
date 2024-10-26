9c66e3b751e4dcb4263fc92b9fe7d5a6c52aae88
Vigo <vigoar@tlen.pl>
2024-10-26 10:09:39 +0200

    sobota 26

--- 05/Pętle.py
+++ 05/Pętle.py
@@ -14,18 +14,28 @@ def ile_wystapien(szukane, lista):
             suma += 1
     return suma
 
-def zakresy_wicia(start, stop, krok, lista):
-    start = lista[start]
-    stop = lista[stop]
+def zakresy_wicia(start, stop, krok):
+    kroki = start
     zakresowo = []
-    #for idc in lista:
-    print(start)
-    print(stop)
-    #zakresowo.append(idc)
+    if krok == 0:
+        return zakresowo
+    if krok > 0:
+        while kroki <= stop:
+            zakresowo.append(kroki)
+            kroki += krok
+    else:
+        while kroki >= stop:
+            zakresowo.append(kroki)
+            kroki += krok
     return zakresowo
 
 if __name__ == "__main__":
-    print (zakresy_wicia (0,20, 5, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,]))
+    print(zakresy_wicia(3, 0, -1))
+    print(zakresy_wicia(-20, -10, 3))
+    print(zakresy_wicia(0, 3, 1))
+    print(zakresy_wicia(10, 14, 1))
+    print(zakresy_wicia(4, 7, 2))
+    #print (zakresy_wicia (0,20, 5, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,]))
     #print(suma_listy([1, 2, 3, 4]))  # 10
     #print(suma_listy([999, 2135, 7845, 11565]))  # 22544
     #print(suma_listy([-7, 11, -4, 20]))  # 20
