cd4a6a4aaf8c4ebe1689306706f74369b398ba24
VigoWB <vigoar@tlen.pl>
2025-03-11 10:40:36 +0100

    z roboty 11-03-2025 slownik 10:40
    Signed-off-by: VigoWB <vigoar@tlen.pl>

--- 06/slownik.py
+++ 06/slownik.py
@@ -1,5 +1,10 @@
-def ile_znakow():
-
+def ile_znakow(napis):
+    importowany = napis
+    ile_ma = len(importowany)
+    doslowny = {}
+    for litery in importowany:
+        doslowny[litery]=litery
+    print(doslowny)
     return
 
 if __name__ == '__main__':
