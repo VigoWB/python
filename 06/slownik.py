cde4a0442ac0d6d7a5e984914ce498402df62d8e
VigoWB <vigoar@tlen.pl>
2025-03-12 15:54:26 +0100

    z roboty 12-03-2025 slowniki  16:10 dodawanie slownikow
    Signed-off-by: VigoWB <vigoar@tlen.pl>

--- 06/slownik.py
+++ 06/slownik.py
@@ -10,6 +10,23 @@ def ile_znakow(napis):
             doslowny[litery] = 1
     return doslowny
 
+def sum_dict(d1, d2):
+    #print(d1,d2)
+    nowy_slowniczek = {}
+    for klucze, wartosci in d1.items():
+        print(klucze,wartosci)
+        for klucz in d2.keys():
+            #print(klucz)
+            if klucze==klucz:
+                nowy_slowniczek[klucze] += wartosci
+            else:
+                nowy_slowniczek[klucze] + d2.keys()
+
+    return d1, d2, nowy_slowniczek
+
 if __name__ == '__main__':
     napis = "ala ma kota"
-    print(ile_znakow(napis))
+    #print(ile_znakow(napis))
+    d1 = ile_znakow(napis)
+    d2 = ile_znakow("jakis napis do sumowania")
+    print(sum_dict(d1, d2))
