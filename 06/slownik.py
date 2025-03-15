9fd9e8e45e68ea5c2d0dc5250d2172b590d0ea32
VigoWB <vigoar@tlen.pl>
2025-03-15 13:19:39 +0100

    Ubu 15-03-2025 13:00 slownik zakresy cyfr

--- 06/slownik.py
+++ 06/slownik.py
@@ -10,6 +10,14 @@ def ile_znakow(napis):
             doslowny[litery] = 1
     return doslowny
 
+
+def generuj_zakres(od, do):
+    zakres = []
+    for cyfra in range(od, do+1):
+        zakres.append(cyfra)
+    return zakres
+
+
 def sum_dict(d1, d2):
     nowy_slowniczek = {}
     nowy_slowniczek.update(d1)
@@ -18,11 +26,15 @@ def sum_dict(d1, d2):
            nowy_slowniczek[klucz] += d2[klucz]
         else:
             nowy_slowniczek[klucz] = d2[klucz]
-    return d1, d2, nowy_slowniczek
+    return nowy_slowniczek
 
 if __name__ == '__main__':
     napis = "ala ma kota"
     #print(ile_znakow(napis))
     d1 = ile_znakow(napis)
     d2 = ile_znakow("JakIS NaPIS Do SUmoWaNIa")
-    print(sum_dict(d1, d2))
+    #print(sum_dict(d1, d2))
+    start_zakresu = 23
+    koniec_zakresu = 34
+    zakres = generuj_zakres(23, 34)
+    print(zakres, ile_znakow(str(zakres)))
