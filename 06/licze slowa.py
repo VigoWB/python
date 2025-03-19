f49214b4c2b61296f8117686215d65985bdc5b7e
VigoWB <vigoar@tlen.pl>
2025-03-19 12:31:23 +0100

    z roboty 19-03-2025 12:30
    Signed-off-by: VigoWB <vigoar@tlen.pl>

--- 06/licze slowa.py
+++ 06/licze slowa.py
@@ -1,11 +1,13 @@
+from slownik import ile_znakow
+
 def importer():
     plik = open('./przyklad.txt')
     tekst = plik.readlines()
-    #for line in tekst:
-       #print(line)
+    for line in tekst:
+       print(line)
     plik.close()
     llini = len(tekst)
-    return llini, plik
+    return llini
 
 
 def licz_slowa():
@@ -16,7 +18,19 @@ def licz_slowa():
     for slowo in podzial:
         ctn += 1
     print(ctn)
-    return podzial, ctn
+    return  ctn
+
+
+def licz_znaki():
+    plik = open('./przyklad.txt')
+    tekst = str(plik.readlines())
+    podzial = tekst.split()
+    znk = ile_znakow(tekst)
+    licznik = 0
+    for ilosc in znk.values():
+        licznik += ilosc
+
+    return licznik
 
 def slownik():
     #rozwiazanie = {}
@@ -27,9 +41,12 @@ def slownik():
 def main():
     #print("Ilość wierszy to: ",importer())
     rozwiazanie = {}
-    rozwiazanie["Plik"] = 21
+    rozwiazanie["Plik"] = "przyklad.txt"
+    rozwiazanie["linie"] = importer()
+    rozwiazanie["wyrazy"] = licz_slowa()
+    rozwiazanie["znaki"] = licz_znaki()
     print(rozwiazanie)
-    print(licz_slowa())
+    #print(licz_slowa())
     return
 
 
