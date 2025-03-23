80810fe63456e38cc0bc8b78f968cb2ffbfbd8f6
VigoWB <vigoar@tlen.pl>
2025-03-23 11:24:18 +0100

    Ubu 123-03-2025 poprawki i sortowanie

--- 06/licze literki.py
+++ 06/licze literki.py
@@ -1,29 +1,41 @@
 
-def imporcik(plik):
+def plik_wczytaj_linie(plik):
     plik = open(plik)
     tekst = plik.readlines()
     plik.close()
     return tekst
 
 def licz_znaki(plik):
-    co_licze = imporcik(plik)
+    co_licze = plik_wczytaj_linie(plik)
     zebrane_dane = {}
     zebrane_dane["slowa"] = 0
     for linia in co_licze:
-        slowo = linia.split()
-        zebrane_dane["slowa"] += len(slowo)
-        for slowo in linia:
-            for znaki in slowo:
-                if znaki == znaki in zebrane_dane:
-                    zebrane_dane[znaki] += 1
+        slowa = linia.split()
+        zebrane_dane["slowa"] += len(slowa)
+        for slowo in slowa:
+            for znak in slowo:
+                if znak in zebrane_dane:
+                    zebrane_dane[znak] += 1
                 else:
-                    zebrane_dane[znaki] = 1
+                    zebrane_dane[znak] = 1
     return zebrane_dane
+
+
+def najczesciej_uzywane_literki(plik):
+    print(licz_znaki(plik))
+    return
+
+
+def najczesciej_uzywane_literki_all(plik):
+    print(licz_znaki(plik))
+    return
 
 def main():
     plik = "przyklad.txt"
     print(licz_znaki(plik))
+    print(najczesciej_uzywane_literki(plik))
     return
+
 
 
 if __name__ == "__main__":
