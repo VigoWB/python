7f4c7615d8d6d1de5ed63570c6366a45be4f642f
VigoWB <vigoar@tlen.pl>
2025-03-31 11:34:22 +0200

    Ubu 31-03-2025 advent d1

---
+++ Advent_2015/Day_1/Day1.py
@@ -0,0 +1,30 @@
+def plik_wczytaj_linie(plik):
+    plik = open(plik)
+    tekst = plik.readlines()
+    plik.close()
+    return tekst
+
+
+def licz_znaki(plik):
+    co_licze = plik_wczytaj_linie(plik)
+    zebrane_dane = {}
+    licznik_slow = 0
+    for linia in co_licze:
+        slowa = linia.split()
+        licznik_slow += len(slowa)
+        for slowo in slowa:
+            for znak in slowo:
+                if znak in zebrane_dane:
+                    zebrane_dane[znak] += 1
+                else:
+                    zebrane_dane[znak] = 1
+    return (zebrane_dane, licznik_slow)
+
+
+def main():
+    plik = "Day1_input.txt"
+    #print(plik_wczytaj_linie(plik))
+    print(licz_znaki(plik))
+
+if __name__ == '__main__':
+    main()
