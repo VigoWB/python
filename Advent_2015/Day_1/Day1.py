158653a65f0c6faada9e89c27cdd3dcbdc324849
VigoWB <vigoar@tlen.pl>
2025-03-31 11:35:12 +0200

    Ubu 31-03-2025 advent d1 v2

--- Advent_2015/Day_1/Day1.py
+++ Advent_2015/Day_1/Day1.py
@@ -8,17 +8,17 @@ def plik_wczytaj_linie(plik):
 def licz_znaki(plik):
     co_licze = plik_wczytaj_linie(plik)
     zebrane_dane = {}
-    licznik_slow = 0
+
     for linia in co_licze:
         slowa = linia.split()
-        licznik_slow += len(slowa)
+
         for slowo in slowa:
             for znak in slowo:
                 if znak in zebrane_dane:
                     zebrane_dane[znak] += 1
                 else:
                     zebrane_dane[znak] = 1
-    return (zebrane_dane, licznik_slow)
+    return zebrane_dane
 
 
 def main():
