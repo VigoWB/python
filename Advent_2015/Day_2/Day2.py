7906638af279b9d566e20693b74e038cb19d4bc4
VigoWB <vigoar@tlen.pl>
2025-04-01 10:28:42 +0200

    Ubu 1-04-2025 advent d1 szkielet

---
+++ Advent_2015/Day_2/Day2.py
@@ -0,0 +1,17 @@
+from Advent_2015.scaffolding.utils import loadFile, loadLines, splitLines
+
+
+def ile_kartonow():
+    plik = splitLines("Day2_input.txt")
+    for linijka in plik:
+        linia_mnozenie = linijka.split('x')
+        for
+
+    return
+
+
+def main():
+    print(ile_kartonow())
+
+if __name__ == '__main__':
+    main()
--- Advent_2015/scaffolding/utils.py
+++ Advent_2015/scaffolding/utils.py
@@ -6,3 +6,7 @@ def loadFile(path):
 def loadLines(path):
     with open(path, 'r') as pointer:
         return pointer.readlines()
+
+def splitLines(path):
+    with open(path, 'r') as pointer:
+        return pointer.read().splitlines()
