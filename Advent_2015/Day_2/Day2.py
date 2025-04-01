05c03babdf47c00707c6a60e0374a0bad9079a6c
VigoWB <vigoar@tlen.pl>
2025-04-01 11:17:23 +0200

    Ubu 1-04-2025 advent d2 v2

--- Advent_2015/Day_2/Day2.py
+++ Advent_2015/Day_2/Day2.py
@@ -4,10 +4,11 @@ from Advent_2015.scaffolding.utils import loadFile, loadLines, splitLines
 def ile_kartonow():
     plik = splitLines("Day2_input.txt")
     for linijka in plik:
-        linia_mnozenie = linijka.split('x')
-        for najmniejszy in linia_mnozenie:
-            print(najmniejszy, min(najmniejszy))
-
+        szer, wys, gleb = [int(opz) for opz in linijka.split('x')]
+        zapas = min(szer, wys, gleb)
+        zapas2 = min((szer*wys),(wys*gleb),(gleb*szer))
+        liczenie = szer*wys+wys*gleb+gleb*szer
+        print(liczenie*2 + zapas2)
     return
 
 
