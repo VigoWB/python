8b40f7d519e9c12d954ad281bd548c00f7da2e4a
VigoWB <vigoar@tlen.pl>
2025-04-02 11:40:20 +0200

    Ubu 2-04-2025 advent d3 szkielet

---
+++ Advent_2015/Day_3/Day3.py
@@ -0,0 +1,15 @@
+from Advent_2015.scaffolding.utils import loadFile, loadLines, splitLines
+
+def i():
+    plik = splitLines("Day3_input.txt")
+
+    return
+
+
+def main():
+    print()
+
+
+
+if __name__ == '__main__':
+    main()
