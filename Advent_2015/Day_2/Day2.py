59a687eea83a2801ef8bc5544999ae9515b6372c
VigoWB <vigoar@tlen.pl>
2025-04-02 10:49:04 +0200

    Ubu 2-04-2025 advent d2 v4

--- Advent_2015/Day_2/Day2.py
+++ Advent_2015/Day_2/Day2.py
@@ -5,13 +5,23 @@ def ile_kartonow():
     suma = 0
     for linijka in plik:
         szer, wys, gleb = [int(opz) for opz in linijka.split('x')]
-        zapas = min((szer*wys),(wys*gleb),(gleb*szer))
-        liczenie = (szer*wys+wys*gleb+gleb*szer)*2
-        suma += liczenie + zapas
+        scianki = [szer*wys,wys*gleb,gleb*szer]
+        suma += (2 * sum(scianki) + min(scianki))
     return suma
+
+
+def wstazki():
+    plik = splitLines("Day2_input.txt")
+    for linijka in plik:
+        szer, wys, gleb = [int(opz) for opz in linijka.split('x')]
+        kokarda = [szer * wys * gleb]
+        wstazka = [min(szer, wys, gleb)]
+        print(kokarda + wstazka)
+    return
+
 
 def main():
     print(ile_kartonow())
-
+    print(wstazki())
 if __name__ == '__main__':
     main()
