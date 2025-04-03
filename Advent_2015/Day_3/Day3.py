0742e5c67b6f22f11b300ad928587622ebd3b364
VigoWB <vigoar@tlen.pl>
2025-04-03 14:20:59 +0200

    Ubu 2-04-2025 advent d3 poczatki2

--- Advent_2015/Day_3/Day3.py
+++ Advent_2015/Day_3/Day3.py
@@ -2,10 +2,26 @@ from Advent_2015.scaffolding.utils import loadFile, loadLines, splitLines
 
 def pozycje():
     plik = loadFile("Day3_input.txt")
+    mapa = {}
+    mapa[(0,0)] = 1
+    #print(mapa[(0,1)])
     for znaczek in plik:
-        kierunek = znaczek.split()
+        #kierunek = znaczek.split()
+        if znaczek in plik == ">":
+            mapa[(0,+1)]
+            if krok in mapa:
+                mapa[krok] = 1
+            else:
+                mapa[krok] += 1
 
-    return
+        if znaczek in plik == "^":
+            mapa[(+1,0)]
+            if krok in mapa:
+                mapa[krok] = 1
+            else:
+                mapa[krok] += 1
+
+    return mapa
 
 
 def main():
