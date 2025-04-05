354e23a4adce116b5b966e68590307cb20a9e642
VigoWB <vigoar@tlen.pl>
2025-04-05 08:37:32 +0200

    Ubu 2-04-2025 advent d3 v7

--- Advent_2015/Day_3/Day3.py
+++ Advent_2015/Day_3/Day3.py
@@ -6,37 +6,26 @@ def pozycje():
     mapa[(0,0)] = 1
     #print(mapa[(0,1)])
     for znaczek in plik:
-        #kierunek = znaczek.split()
-        if znaczek in plik == ">":
-            krok = mapa[(0,+1)]
-            if krok==krok in mapa:
-                mapa[krok] += 1
-            else:
-                mapa[krok] = 1
-
-    return mapa
-
-def kierunek():
-    plik = loadFile("Day3_input.txt")
-    mapa = {}
-    mapa[0, 0] = 1
-    for znaczek in plik:
-        if znaczek == ">":
-            mapa[0, +1] = "prawo"
-            continue
-        if znaczek == "<":
-            mapa[0, -1] = "lewo"
-            continue
-        if znaczek == "^":
-            mapa[-1, 0] = "gora"
-            continue
-        if znaczek == "v":
-            mapa[+1, 0] = "dol"
+        ruch = kierunek(znaczek)
+        if mapa[(ruch)] == mapa[(0,0)]:
+            mapa[(ruch)] += 1
+        else:
             continue
     return mapa
+
+def kierunek(kierunek: str) -> (int, int):
+    if kierunek == ">":
+        return 1, 0
+    if kierunek == "<":
+        return -1, 0
+    if kierunek == "^":
+        return 0, 1
+    if kierunek == "v":
+        return 0, -1
+
 
 def main():
-    #print(pozycje())
+    print(pozycje())
     print(kierunek())
 
 
