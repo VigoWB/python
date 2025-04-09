97dadae3513f18434691369eb06c7b46578b20a5
VigoWB <vigoar@tlen.pl>
2025-04-09 19:47:49 +0200

    Ubu 9-04-2025 advent d5 v1

--- Advent_2015/Day_5/Day5.py
+++ Advent_2015/Day_5/Day5.py
@@ -2,7 +2,15 @@ from Advent_2015.scaffolding.utils import loadFile, loadLines, splitLines
 
 def os():
     plik = splitLines("Day5_input.txt")
-    return plik
+    szukane_znaki = ["ab", "cd", "pq", "xy"]#'ab' or 'cd' or 'pq' or 'xy'
+    for linia in plik:
+        for literki in szukane_znaki:
+            if literki in linia:
+                continue
+            else:
+                print(f"Nie znaleziono znaczkow: {linia}")
+                #jak to wyswietlic raz?
+    return
 
 
 # def po():
