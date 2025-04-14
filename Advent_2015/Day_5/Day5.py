0781887beef9c8b58750824680fde5652db013b4
VigoWB <vigoar@tlen.pl>
2025-04-14 12:43:18 +0200

    z Roboty 14-04-2025 advent d5 v5

--- Advent_2015/Day_5/Day5.py
+++ Advent_2015/Day_5/Day5.py
@@ -3,17 +3,12 @@ from Advent_2015.scaffolding.utils import loadFile, loadLines, splitLines
 def szukanie_ciagu():
     plik = splitLines("Day5_input.txt")
     szukane_znaki = ["ab", "cd", "pq", "xy"]#'ab' or 'cd' or 'pq' or 'xy'
-    znalezione_bez = [linia.strip() for linia in plik if not any(ciagi in linia for ciagi in szukane_znaki)]
-    znalezione_z = [linia.strip() for linia in plik if any(ciagi in linia for ciagi in szukane_znaki)]
-    # for linia in plik:
-    #     for ciagi in szukane_znaki:
-    #         if not ciagi in linia:
-    #             znalezione_bez += [linia]
-    #         else:
-    #             znalezione_z += [linia]
-    #             continue
-                #print(f"    Nie znaleziono znaczkow: {linia}")
-    return znalezione_bez, znalezione_z
+    for linia in plik:
+        for ciagi in szukane_znaki:
+             if ciagi in linia:
+                 return False
+    return True
+
 
 
 def bez_ciagu(znalezione_bez):
