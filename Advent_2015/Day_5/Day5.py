f642d1b4c7178532e1ad6a75c630c1bb6e3c3c4f
VigoWB <vigoar@tlen.pl>
2025-04-15 20:37:41 +0200

    Ubu 9-04-2025 advent d5 v10

--- Advent_2015/Day_5/Day5.py
+++ Advent_2015/Day_5/Day5.py
@@ -1,8 +1,8 @@
 from Advent_2015.scaffolding.utils import loadFile, loadLines, splitLines
 
-def szukanie_ciagu():
-    plik = splitLines("Day5_input.txt")
-    szukane_znaki = ["ab", "cd", "pq", "xy"]#'ab' or 'cd' or 'pq' or 'xy'
+def szukanie_ciagu(napis):
+    plik = napis# splitLines("Day5_input.txt")
+    szukane_znaki = ["ab", "cd", "pq", "xy"]
     for linia in plik:
         for ciagi in szukane_znaki:
              if ciagi in linia:
@@ -18,17 +18,16 @@ def nice(napis: str) -> bool:
     samogloski = ["a","e","i","o","u"]
     podwojne = False
     licze = 0
-
-    for literka in napis:
-        if literka in samogloski:
-            licze += 1
-
+    for linia in napis:
+        for literka in linia:
+            if literka in samogloski:
+                licze += 1
 
     #podwojne znaki
-    for litera in range(len(napis) - 1):
-        if napis[litera] == napis[litera + 1]:
-            podwojne = True
-
+    for linia in napis:
+        for litera in range(len(linia) - 1):
+            if napis[litera] == napis[litera + 1]:
+                podwojne = True
 
     wiersz = False
     if podwojne == True and licze >= 3:
@@ -37,10 +36,13 @@ def nice(napis: str) -> bool:
     return wiersz
 
 
+
+
 def main():
-    napis = 'fujcauyucsrxxgatisb'
-    #print("ciagi ", szukanie_ciagu())
-    print("najs: ",napis, nice(napis))
+    napis = splitLines('Day5_input.txt')
+    #napis = 'fujcauyucsrxxgatisb'
+    print("ciagi ", szukanie_ciagu(napis))
+    print("najs: ", nice(napis))
 
 
 if __name__ == '__main__':
