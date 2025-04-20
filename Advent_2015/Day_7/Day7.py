cbf6967339a9cc15b742501288e74fb03a7dd927
VigoWB <vigoar@tlen.pl>
2025-04-20 11:39:21 +0200

    Ubu 20-04-2025 advent d7 szkielet

---
+++ Advent_2015/Day_7/Day7.py
@@ -0,0 +1,16 @@
+from Advent_2015.scaffolding.utils import splitLines
+
+
+def wczytaj():
+    plik = splitLines('Day7_input.txt')
+    return plik
+
+
+def main():
+    print(wczytaj())
+    return
+
+
+
+if __name__ == '__main__':
+    main()
