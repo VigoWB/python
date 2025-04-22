1485c87706e7d1e1156bf7b856af6cd249af1904
VigoWB <vigoar@tlen.pl>
2025-04-22 07:19:48 +0200

    Ubu 22-04-2025 advent d7 poczatki

--- Advent_2015/Day_7/Day7.py
+++ Advent_2015/Day_7/Day7.py
@@ -1,9 +1,20 @@
+from Advent_2015.Day_7.instrukcje import Instrukcja
 from Advent_2015.scaffolding.utils import splitLines
 
 
 def wczytaj():
-    plik = splitLines('Day7_input.txt')
-    return plik
+    linie = splitLines('Day7_input.txt')
+    instrukcje: list[Instrukcja] = []
+    for linia in linie:
+        instrukcje.append(Instrukcja(linia))
+    return instrukcje
+
+def main():
+    instrukcje = wczytaj()
+    # print(smart_fib(50))
+    # for i in range(0, 5):
+    #     print(f"FIB {i} - {fib(i)}")
+    # return
 
 
 pamiec = [0 for i in range(10_000)]
@@ -15,22 +26,11 @@ def smart_fib(n: int) -> int:
         pamiec[idx] = pamiec[idx-1] + pamiec[idx-2]
     return pamiec[n]
 
-
-
 def fib(n):
     print(f"fib({n})")
     if n == 0 or n == 1:
         return 1
     return fib(n-1) + fib(n-2)
-
-def main():
-    #print(wczytaj())
-    print(smart_fib(50))
-    # for i in range(0, 5):
-    #     print(f"FIB {i} - {fib(i)}")
-    # return
-
-
 
 if __name__ == '__main__':
     main()
--- Advent_2015/Day_7/instrukcje.py
+++ Advent_2015/Day_7/instrukcje.py
@@ -3,8 +3,8 @@
 class Instrukcja:
     def __init__(self, linia: str) -> None:
         self.operacja = None
-        # self.start = (0, 0)
-        # self.stop = (999, 999)
+        self.start = (0, 0)
+        self.stop = (999, 999)
         self.parse(linia)
 
 
@@ -13,22 +13,20 @@ class Instrukcja:
         # Przykład: "turn on 931,331 through 939,812"
         # -> ['turn', 'on', '931,331', 'through', '939,812']
 
-        czesci = linia.strip().split(->)
+        czesci = linia.strip().split()
 
         # Operacja może być jedno- lub dwuwyrazowa ("turn on", "turn off", "toggle")
-        if czesci[0] <= 3:
-            self.operacja = "toggle"
-            start_str = czesci[1]
-            stop_str = czesci[3]
-        else:
-            # "turn on" lub "turn off"
-            self.operacja = f"{czesci[0]} {czesci[1]}"
-            start_str = czesci[2]
-            stop_str = czesci[4]
+        if "AND" in czesci:
+            print("TU JEST AND")
+        # else:
+        #     # "turn on" lub "turn off"
+        #     self.operacja = f"{czesci[0]} {czesci[1]}"
+        #     start_str = czesci[2]
+        #     stop_str = czesci[4]
 
         # Parsowanie współrzędnych start i stop
-        self.start = tuple(map(int, start_str.split(',')))
-        self.stop = tuple(map(int, stop_str.split(',')))
+        # self.start = tuple(map(int, start_str.split(',')))
+        # self.stop = tuple(map(int, stop_str.split(',')))
 
     def __repr__(self):
         return f'{self.operacja}, {self.start}, {self.stop}' #f"Instrukcja (operacja: {self.operacja}, start->{self.start}, stop->{self.stop})"
