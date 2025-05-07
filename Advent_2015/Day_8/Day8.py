6a272713c3130ab95d315d0c0f6b462531aac9e6
VigoWB <vigoar@tlen.pl>
2025-05-07 15:31:26 +0200

    z Roboty advent d8

--- Advent_2015/Day_8/Day8.py
+++ Advent_2015/Day_8/Day8.py
@@ -1,17 +1,17 @@
-from Advent_2015.Day_7.instrukcje import Instrukcja
 from Advent_2015.scaffolding.utils import splitLines
 
-
-def wczytaj():
-    linie = splitLines('Day8_input.txt')
-    return linie
-
-
 def wykonaj():
-    return
+    linijka = splitLines('Day8_input.txt')
+    znaki = 0
+    litery = 0
+    for linia in linijka:
+        znaki += len(linia)
+        litery += sum(c.isalpha() for c in linia)
+    return znaki, litery
 
 def main():
-    print(wczytaj())
+    # print(wczytaj())
+    print(wykonaj())
 
 if __name__ == '__main__':
     main()
