5d5fd4491446a6cb3934607fa34dfee3296ebefc
VigoWB <vigoar@tlen.pl>
2025-05-07 16:21:51 +0200

    z Roboty advent d8

--- Advent_2015/Day_8/Day8.py
+++ Advent_2015/Day_8/Day8.py
@@ -1,13 +1,19 @@
 from Advent_2015.scaffolding.utils import splitLines
 
 def wykonaj():
-    linijka = splitLines('Day8_input.txt')
+    tekst = splitLines('Day8_input.txt')
     znaki = 0
     litery = 0
-    for linia in linijka:
+    for linia in tekst:
         znaki += len(linia)
         litery += sum(c.isalpha() for c in linia)
-    return znaki, litery
+        if '\"' in linia:
+            print(f"tu jest slasz ", {linia})
+        if '\\' in linia:
+            print(f"tu jest 2x slasz'", {linia})
+        # if '\x' in linia:
+        #     print(f"tu jest \ x", {linia})
+    return
 
 def main():
     # print(wczytaj())
