7653a2676c2fa53d6525f087f0372b307b3a6d5c
VigoWB <vigoar@tlen.pl>
2025-05-09 10:54:05 +0200

    z Roboty advent d8

--- Advent_2015/Day_8/Day8.py
+++ Advent_2015/Day_8/Day8.py
@@ -4,27 +4,26 @@ def wczytaj():
     linia = splitLines('Day8_input.txt')
     return linia
 
+
+
 def wykonaj(linia: str)->tuple[int, int]:
     tekst = linia
-    znaki = 0
-    litery = 0
     for linia in tekst:
-        print(linia, len(linia))
-        for znak in linia:
-            if '\\' in linia:
-                continue
-            print(znak)
-            znaki += 1
-        # znaki += len(linia)
-        # litery += sum(c.isalpha() for c in linia)
+        res = policz(linia)
+        print(linia, len(linia), res)
+    return 1, 1 #tu beda kiedys wyniki
 
-        # if 'c' in linia:
-        #     if linia.index('c') + 2 < len(linia):
-        #         print(ord('\\'), format(ord('\\'), '08b')) # kod ASCII i konwersja na 8-bitowy binarny string
-        #         print(f"",linia[linia.index('c') + 2])
-        #     if linia.index('c') + 2 >= len(linia):
-        #         print('za dlugie')
-    return znaki, litery
+
+
+def policz(linia: str)->int:
+    znaki = 0
+    for znak in linia[1:-1]:
+        if znak == '\\':
+            continue
+        znaki += 1
+    return znaki
+
+
 
 def main():
     wczytaj()
