e2382084f03a18b403659d4bdc6c758a87ec3bd1
VigoWB <vigoar@tlen.pl>
2025-05-12 16:28:14 +0200

    z Roboty advent d8

--- Advent_2015/Day_8/Day8.py
+++ Advent_2015/Day_8/Day8.py
@@ -10,8 +10,11 @@ def wykonaj(linia: str)->tuple[int, int]:
     litery = 0
     res = 0
     for linia in tekst:
-        res += policz(linia[1:-1])
-        litery += len(linia)
+        tres = policz(linia[1:-1])
+        res += tres
+        tlitery = len(linia)
+        litery += tlitery
+        print(linia, tlitery, tres)
         # print(linia, len(linia), res)
     return res, litery
 
@@ -22,23 +25,26 @@ def policz(linia: str)->int:
     for pozycja, znak in enumerate(linia):
         if znak == '\\':
             if isescape(linia[pozycja:pozycja + 4]):
-                print(f"test ZDANy ", linia)
+                # print(f"test ZDANy ", linia)
                 znaki += 4
                 # print(isescape(linia[pozycja:pozycja+4]))
-            if not isescape(linia[pozycja:pozycja + 4]):
-                print(f"test niezdany ", linia)
-                znaki += 2
+            else:
+                # print(f"test niezdany ", linia)
+                znaki += 1
             continue
     return znaki
 
 def isescape(linia: str) -> bool:
-    print(f'TESTY LINI: ', linia)
-    if linia[0] == '\\' and linia[1] == 'x' and len(linia) == 4:
-        # if linia[-2] == znak in range(0,9) and linia[-1] == range('a-f'):
-        hex_digits = '0123456789abcdefABCDEF' # sciagnolem z AI
-        return linia[2] in hex_digits and linia[3] in hex_digits
-    else:
+    if linia[1] != 'x':
         return False
+    if len(linia) != 4:
+        return False
+    hex_digits = '0123456789abcdef'
+    if str.lower(linia[2]) not in hex_digits:
+        return False
+    if str.lower(linia[3]) not in hex_digits:
+        return False
+    return True
 
 
 
