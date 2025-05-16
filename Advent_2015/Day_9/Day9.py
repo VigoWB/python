ac2f65a05b28aa64e48a2d0a23bae8caa702ef7f
VigoWB <vigoar@tlen.pl>
2025-05-16 14:36:45 +0200

    z Roboty advent d9

--- Advent_2015/Day_9/Day9.py
+++ Advent_2015/Day_9/Day9.py
@@ -1,42 +1,36 @@
 from Advent_2015.scaffolding.utils import splitLines
+from Advent_2015.Day_9.klasaDay_9 import mapki
 
 def wczytaj():
     linia = splitLines('Day9_input.txt')
     return linia
 
-def wykonaj(linie: str):
-    tekst = linie
+def wykonaj(linie: list[str]) -> dict[str, int]:
     miasta = {}
-
-    for linia in tekst:
+    numer = 1
+    for linia in linie:
         dane = podzial(linia)
-        for asd in dane:
-            if asd[0] not in miasta:
-                numer = len(miasta)
-                miasta[asd[0]] = numer + 1
-            else:
-                pass
+        for (skad, dokad) in dane.keys():
+            if skad not in miasta:
+                miasta[skad] = numer
+                numer += 1
+            if dokad not in miasta:
+                miasta[dokad] = numer
+                numer += 1
+    return miasta
 
-
-    print(len(miasta),miasta)
-    return
-
-def podzial(linie: list[str])->dict[tuple[str, str], int]:
-    polaczenia ={}
-    miasta = {}
-    linia = linie.split()
-    for slowa in linia:
-        zkad = linia[0]
-        dokad = linia[2]
-        odleglosc = int(linia[-1])
-        polaczenia[(zkad, dokad)] = odleglosc
-        polaczenia[(dokad, zkad)] = odleglosc
-    return polaczenia
+def podzial(linia: str) -> dict:
+    slowa = linia.split()
+    zkad = slowa[0]
+    dokad = slowa[2]
+    odleglosc = int(slowa[-1].lstrip('='))
+    return {(zkad, dokad): odleglosc, (dokad, zkad): odleglosc}
 
 
 
 
 def main():
+
     print(wykonaj(wczytaj()))
 
 
---
+++ Advent_2015/Day_9/klasaDay_9.py
@@ -0,0 +1,23 @@
+class mapki:
+    def __init__(self, linia: str):
+        self.miasta: dict = {}
+        self.polaczenia: dict = {}
+
+    def dodaj(self, nazwa: str) -> int:
+        if nazwa not in self.miasta.keys():
+            lepszanazwa = len(self.miasta)
+            self.miasta[nazwa] = lepszanazwa
+            return lepszanazwa
+        else:
+            return self.miasta.get(nazwa)
+
+    def znajdz(self, nazwa: str) -> int:
+        if nazwa in self.miasta.keys():
+            return self.miasta[nazwa]
+        else:
+            return -1
+
+
+
+    # def __repr__(self):
+    #     return f'{self.operacja}, {self.argumenty}, {self.wynik}'
