8d4d1cc2c761baf9879ee37aa87be7bae11bdab4
Krzysztof Czechowski <krzysztof.czechowski@b-mind.pl>
2025-06-22 11:54:12 +0200

    flask: docker-compose poprawione porty

--- dock-python/python/app.py
+++ dock-python/python/app.py
@@ -1,10 +1,18 @@
+import time
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Co do chuja aaa?"
+
+
+@app.route("/time")
+def time():
+    return {'time': time.time()}
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
