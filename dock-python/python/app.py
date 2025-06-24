99f01b4d0c46fa93a1fd234cd7ed49a700510f7b
VigoWB <vigoar@tlen.pl>
2025-06-24 16:13:56 +0200

    z Roboty dokery

--- dock-python/python/app.py
+++ dock-python/python/app.py
@@ -8,15 +8,22 @@ app = Flask(__name__)
 def hello():
     return "Co do chuja aaa?"
 
+
 @app.route("/hello")
 def BU():
     return "Witaj wielki BU"
+
 
 @app.route('/potega/<int:liczba>')
 def potega(liczba):
     wynik = liczba ** 2
     wynikczy = liczba ** 3
     return f"{liczba} do potęgi: {wynik}, a do 3 gratis {wynikczy}"
+
+
+@app.route('/lists', methods=['GET'])
+def lista():
+    return "TO JEST LISTA LIST"
 
 
 @app.route("/time")
