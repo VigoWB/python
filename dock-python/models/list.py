2d53f7a92a49edbd00dd91a73b59789a2afbc6cd
VigoWB <vigoar@tlen.pl>
2025-06-25 10:17:22 +0200

    z Roboty list

---
+++ dock-python/models/list.py
@@ -0,0 +1,23 @@
+
+
+class ListaNotatek:
+    def __init__(self):
+        self.id = 20
+        self.title: str = ''
+        self.created_at: str = "2020-05-02 10:00:00"
+        self.updated_at: str = "2023-09-15 15:22:33"
+        self.pin = True
+        self.color: str = 'red'
+        pass
+
+
+
+    def toResponse(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+            "pin": self.pin,
+            "color": self.color,
+        }
