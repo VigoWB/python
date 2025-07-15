0a850dcc3b320e836ce418d5c77ac05783b7fb85
VigoWB <vigoar@tlen.pl>
2025-07-15 10:52:10 +0200

    z Roboty django

---
+++ djangonauki/app/moj_projekt/wsgi.py
@@ -0,0 +1,17 @@
+# moj_projekt/wsgi.py
+"""
+WSGI config for moj_projekt project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moj_projekt.settings')
+
+application = get_wsgi_application()
