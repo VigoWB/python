a49603f6bfb557ac620f76caf5982376ff7cdb4f
VigoWB <vigoar@tlen.pl>
2025-08-21 12:52:30 +0200

    z Roboty django

--- djangonauki/src/moj_projekt/models.py
+++ djangonauki/src/moj_projekt/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-#from django.utils import timezone
+from django.utils import timezone
 
 class Item(models.Model):
     content = models.TextField()
-#    date_posted = models.DateTimeField(default=timezone.now)
+    date_posted = models.DateTimeField(default=timezone.now)
